50cd8208de5c4120eafb8167454d5e39c8693893
Michail Pomazkin <mpomazkin9@gmail.com>
2026-04-04 22:34:36 +0500

    Реализация модуля оценки качества инверсии: добавлены метрики PSNR, SSIM и LPIPS

---
+++ metrics/evaluators.py
@@ -0,0 +1,53 @@
+import torch
+import numpy as np
+from PIL import Image
+import torch.nn.functional as F
+from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+from lpips import LPIPS
+
+class ImageInversionEvaluator:
+    """
+    Класс для оценки качества инверсии изображений.
+    Содержит методы для вычисления математических метрик.
+    """
+    def __init__(self, device="cuda"):
+        self.device = device
+        # Инициализируем метрики
+        self.psnr = PeakSignalNoiseRatio().to(device)
+        self.ssim = StructuralSimilarityIndexMeasure().to(device)
+        # LPIPS требует загрузки весов (vgg по умолчанию)
+        self.lpips_metric = LPIPS(net='vgg').to(device)
+
+    def preprocess(self, pil_image: Image.Image) -> torch.Tensor:
+        """Приводит PIL изображение к тензору [1, 3, H, W] в диапазоне [0, 1]."""
+        img = np.array(pil_image).astype(np.float32) / 255.0
+        img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
+        return img.to(self.device)
+
+    def calculate_metrics(self, original: Image.Image, reconstructed: Image.Image):
+        """
+        Считает полный набор метрик для пары изображений.
+        """
+        img1 = self.preprocess(original)
+        img2 = self.preprocess(reconstructed)
+
+        # 1. PSNR (выше - лучше)
+        psnr_val = self.psnr(img1, img2).item()
+
+        # 2. SSIM (ближе к 1 - лучше)
+        ssim_val = self.ssim(img1, img2).item()
+
+        # 3. LPIPS (ближе к 0 - лучше)
+        # LPIPS ожидает вход в диапазоне [-1, 1]
+        img1_lpips = img1 * 2.0 - 1.0
+        img2_lpips = img2 * 2.0 - 1.0
+        lpips_val = self.lpips_metric(img1_lpips, img2_lpips).item()
+
+        # 4. MSE (ниже - лучше)
+        mse_val = F.mse_loss(img1, img2).item()
+
+        return {
+            "psnr": psnr_val,
+            "ssim": ssim_val,
+            "lpips": lpips_val,
+            "mse": mse_val
